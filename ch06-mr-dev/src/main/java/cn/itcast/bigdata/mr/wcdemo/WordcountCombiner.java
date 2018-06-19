7e6db96c053d6b35ffc0ec6aa0a9258022d1d700
zzy <zzy1120716@126.com>
2018-06-19 19:58:05 +0800

    combiner

---
+++ ch06-mr-dev/src/main/java/cn/itcast/bigdata/mr/wcdemo/WordcountCombiner.java
@@ -0,0 +1,20 @@
+package cn.itcast.bigdata.mr.wcdemo;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class WordcountCombiner extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        int count = 0;
+        for (IntWritable v : values) {
+            count += v.get();
+        }
+
+        context.write(key, new IntWritable(count));
+    }
+}
--- ch06-mr-dev/src/main/java/cn/itcast/bigdata/mr/wcdemo/WordcountDriver.java
+++ ch06-mr-dev/src/main/java/cn/itcast/bigdata/mr/wcdemo/WordcountDriver.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.CombineFileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
@@ -18,8 +19,19 @@ public class WordcountDriver {
     public static void main(String[] args) throws Exception {
 
         Configuration conf = new Configuration();
-        /*conf.set("mapreduce.framework.name", "yarn");
-        conf.set("yarn.resourcemanager.hostname", "mini1");*/
+
+        // 是否运行为本地模式，就是看这个参数值是否为local，默认就是local
+        /*conf.set("mapreduce.framework.name", "local");*/
+
+        // 到底在哪里，就看以下两行配置用哪行，默认就是file:///
+        /*conf.set("fs.defaultFS", "hdfs://mini1:9000/");*/
+        /*conf.set("fs.defaultFS", "file:///");*/
+
+        // 运行集群模式，就是把程序提交到yarn中去运行
+        // 要想运行为集群模式，以下3个参数要指定为集群上的值
+        conf.set("mapreduce.framework.name", "yarn");
+        conf.set("yarn.resourcemanager.hostname", "mini1");
+        conf.set("fs.defaultFS", "hdfs://mini1:9000/");
         Job job = Job.getInstance(conf);
 
         /*job.setJar("/home/hadoop/wc.jar");*/
@@ -38,6 +50,17 @@ public class WordcountDriver {
         // 指定最终输出的数据的kv类型
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
+
+
+        // 指定需要使用combiner，以及用哪个类作为combiner的逻辑
+        /*job.setCombinerClass(WordcountCombiner.class);*/
+        job.setCombinerClass(WordcountReducer.class);
+
+
+        // 如果不设置InputFormat，它默认用的是TextInputFormat.class
+        job.setInputFormatClass(CombineFileInputFormat.class);
+        CombineFileInputFormat.setMaxInputSplitSize(job, 4194304);
+        CombineFileInputFormat.setMinInputSplitSize(job, 2097152);
 
         // 指定job的输入原始文件所在目录
         FileInputFormat.setInputPaths(job, new Path(args[0]));
